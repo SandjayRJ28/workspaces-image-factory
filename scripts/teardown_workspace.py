cf3252bdc03551d95af98d679488b915e0574350
SandjayRJ28 <62304094+SandjayRJ28@users.noreply.github.com>
2022-12-21 17:26:36 +0100

    Update teardown_workspace.py
    retaint resource

--- scripts/teardown_workspace.py
+++ scripts/teardown_workspace.py
@@ -2,7 +2,7 @@ import boto3
 
 def main():
     cloudformation = boto3.client('cloudformation')
-    response = cloudformation.delete_stack(StackName='WorkspaceBuilder', RetainResources=['workspace1'])
+    response = cloudformation.delete_stack(StackName='WorkspaceBuilder', RetainResources=[WorkspaceId])
 
 
 if __name__ == "__main__":
